ad01eec47c00c56c02263e986964f12af5a00d90
Edu2032 <eduardo.oliveiraramos12@hotmail.com>
2023-04-08 12:48:57 -0300

    button

--- frontend/src/components/HomeCard/index.tsx
+++ frontend/src/components/HomeCard/index.tsx
@@ -1,11 +1,16 @@
-
+import HomeCardButtom from "../HomeCardButtom";
 import "./styles.css";
 const HomeCard = () => {
-    return (
+  return (
+    <>
       <nav className="container home-container">
         <h1>Desafio GitHub API</h1>
         <h5>Desenvolvimento de aplicativo para integrar com API</h5>
       </nav>
-    );
-  };
-  export default HomeCard;
+      <nav className="button-container">
+        <HomeCardButtom />
+      </nav>
+    </>
+  );
+};
+export default HomeCard;
---
+++ frontend/src/components/HomeCardButtom/index.tsx
@@ -0,0 +1,8 @@
+const HomeCardButtom = () => {
+  return (
+    <nav className="container button-tex">
+      <button className="btn btn-primary">COMEÇAR</button>
+    </nav>
+  );
+};
+export default HomeCardButtom;
