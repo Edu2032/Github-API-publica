089354388053f5fea034925bd00ab4e8fb64f612
Edu2032 <eduardo.oliveiraramos12@hotmail.com>
2023-04-08 11:43:00 -0300

    navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <h1>Olá</h1>;
+  return (
+    <>
+      <NavBar />
+      <h1>Olá</h1>
+    </>
+  );
 }
 
 export default App;
--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,4 +1,12 @@
+import "./styles.css";
 const NavBar = () => {
-
-}
+  return (
+    <nav className="navbar bg-primary main-nav">
+      <div className="container class-primary">
+        <h4>Github API</h4>
+        <div></div>
+      </div>
+    </nav>
+  );
+};
 export default NavBar;
